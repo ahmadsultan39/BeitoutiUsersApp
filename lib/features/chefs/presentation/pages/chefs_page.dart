ad58139ec0c7d866a7c8828574500a28dfdd371b
hamza.a <hamza.a@appagroup.net>
2022-08-08 14:07:41 +0300

    fix chefs page bloc

--- lib/features/chefs/presentation/pages/chefs_page.dart
+++ lib/features/chefs/presentation/pages/chefs_page.dart
@@ -6,53 +6,64 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/util/constants.dart';
 
-class ChefsPage extends StatelessWidget {
+class ChefsPage extends StatefulWidget {
   const ChefsPage({Key? key}) : super(key: key);
 
   @override
+  State<ChefsPage> createState() => _ChefsPageState();
+}
+
+class _ChefsPageState extends State<ChefsPage> {
+  final _bloc = sl<ChefsBloc>();
+
+  @override
+  void initState() {
+    _bloc.addGetMostRecentChefsEvent();
+    _bloc.addGetNearestChefsEvent();
+    _bloc.addGetTopOrderedChefsEvent();
+    _bloc.addGetTopRatedChefsEvent();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final bloc = sl<ChefsBloc>();
-    bloc.addGetMostRecentChefsEvent();
-    bloc.addGetNearestChefsEvent();
-    bloc.addGetTopOrderedChefsEvent();
-    bloc.addGetTopRatedChefsEvent();
     return BlocBuilder<ChefsBloc, ChefsState>(
-      bloc: bloc,
+      bloc: _bloc,
       builder: (context, state) {
         WidgetsBinding.instance?.addPostFrameCallback((_) {
           message(
             message: state.message,
             isError: state.error,
             context: context,
-            bloc: bloc,
+            bloc: _bloc,
           );
         });
-        return  SingleChildScrollView(
-            child: Column(
-              children: [
-                ChefsList(
-                  title: "أقرب الطهاة",
-                  isLoading: state.isNearestLoading,
-                  chefs: state.nearest,
-                ),
-                ChefsList(
-                  title: "الطهاة الأعلى تقييما",
-                  isLoading: state.isTopRatedLoading,
-                  chefs: state.topRated,
-                ),
-                ChefsList(
-                  title: "الطهاة الأكثر طلبا",
-                  isLoading: state.isTopOrderedLoading,
-                  chefs: state.topOrdered,
-                ),
-                ChefsList(
-                  title: "أحدث الطهاة",
-                  isLoading: state.isMostRecentLoading,
-                  chefs: state.mostRecent,
-                ),
-              ],
-            ),
-          );
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              ChefsList(
+                title: "أقرب الطهاة",
+                isLoading: state.isNearestLoading,
+                chefs: state.nearest,
+              ),
+              ChefsList(
+                title: "الطهاة الأعلى تقييما",
+                isLoading: state.isTopRatedLoading,
+                chefs: state.topRated,
+              ),
+              ChefsList(
+                title: "الطهاة الأكثر طلبا",
+                isLoading: state.isTopOrderedLoading,
+                chefs: state.topOrdered,
+              ),
+              ChefsList(
+                title: "أحدث الطهاة",
+                isLoading: state.isMostRecentLoading,
+                chefs: state.mostRecent,
+              ),
+            ],
+          ),
+        );
       },
     );
   }
